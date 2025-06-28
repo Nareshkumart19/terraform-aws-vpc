1ed53a125d0061354f9a5036581b21ee06d3cb18
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-06-28 21:36:15 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -7,10 +7,10 @@ output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
 #
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
-
-output "database_subnet_ids" {
-  value = aws_subnet.database[*].id
-}
+#output "private_subnet_ids" {
+#  value = aws_subnet.private[*].id
+#}
+#
+#output "database_subnet_ids" {
+#  value = aws_subnet.database[*].id
+#}
